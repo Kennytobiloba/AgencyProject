a7f9b79478f32cb8b31e8c47a4cc06a839e75ae0
kennytobiloba <olaatunbikehinde@gmail.com>
2024-01-20 00:57:16 +0100

    change

--- src/Container/Info/Info.jsx
+++ src/Container/Info/Info.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const Info = () => {
   return (
     <div className=' px-[2rem] sm:px-[3rem] pb-[8rem]   lg:px-[10rem]'  >
-       <div className=' lg:w-1/2 w-full xl:mx-auto text-center'>
-        <h1 className=' mb-8 text-gray-800 lg:text-6xl  text-3xl font-semibold font-[Inter] lg:leading-[70px] '>Pellentesque suscipit fringilla libero eu.</h1>
+       <div className=' xl:w-1/2 lg:3/5 w-full xl:mx-auto text-center 2xl:w-[60%]'>
+        <h1 className=' mb-8 text-gray-800 lg:text-6xl  text-3xl font-semibold font-[Inter] lg:leading-[70px]  '>Pellentesque suscipit fringilla libero eu.</h1>
 
         <div className=' bg-green-500 px-4 py-4  justify-center items-center inline-flex'>
             <a href="" className='text-center text-white text-base font-medium font-[Inter] leading-normal'>Get a Demo</a>
@@ -13,6 +13,7 @@ const Info = () => {
        </div>
 
     </div>
+
   )
 }
 
